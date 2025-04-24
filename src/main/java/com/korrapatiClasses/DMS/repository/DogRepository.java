4cff8fe3847588258c97f245ed32882bded9d625
venkata kishore korrapati <s575100@nwmissouri.edu>
2025-04-24 15:32:13 -0500

    Intial Commit

---
+++ src/main/java/com/korrapatiClasses/DMS/repository/DogRepository.java
@@ -0,0 +1,13 @@
+package com.korrapatiClasses.DMS.repository;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.korrapatiClasses.DMS.Models.Dog;
+
+public interface DogRepository extends CrudRepository<Dog, Integer> {
+
+	List<Dog> findByName(String name);
+
+}
---
+++ src/main/java/com/korrapatiClasses/DMS/repository/TrainerRepository.java
@@ -0,0 +1,9 @@
+package com.korrapatiClasses.DMS.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.korrapatiClasses.DMS.Models.Trainer;
+
+public interface TrainerRepository extends CrudRepository<Trainer, Integer> {
+
+}
